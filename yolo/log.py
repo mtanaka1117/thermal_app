7a848fba2f35fbbd056024cf996faf1eebc5b32e
mtanaka1117 <u710058f@ecs.osaka-u.ac.jp>
2023-10-29 16:24:22 +0900

    edit log_video.py

--- yolo/log.py
+++ yolo/log.py
@@ -6,16 +6,17 @@ from ultralytics import YOLO
 from matplotlib import pyplot as plt
 import datetime
 from collections import deque, Counter
+import pandas as pd
 
-b_thermal_path = r'C:\Users\tnkmo\Downloads\items1\items1\20230807_1313\20230807_131354592_T.jpg'
-a_thermal_path = r'C:\Users\tnkmo\Downloads\items1\items1\20230807_1313\20230807_131358192_T.jpg'
+# b_thermal_path = r'C:\Users\tnkmo\Downloads\items1\items1\20230807_1313\20230807_131354592_T.jpg'
+# a_thermal_path = r'C:\Users\tnkmo\Downloads\items1\items1\20230807_1313\20230807_131358192_T.jpg'
 
-visible_path = r'C:\Users\tnkmo\Downloads\items1\items1\20230807_1313\20230807_131358202_V.jpg'
+# visible_path = r'C:\Users\tnkmo\Downloads\items1\items1\20230807_1313\20230807_131358202_V.jpg'
 
-# b_thermal_path = "/home/srv-admin/images/items1/1313/20230807_131354592_T.jpg"
-# a_thermal_path = "/home/srv-admin/images/items1/1313/20230807_131358192_T.jpg"
+b_thermal_path = "/home/srv-admin/images/items1/1313/20230807_131354592_T.jpg"
+a_thermal_path = "/home/srv-admin/images/items1/1313/20230807_131358192_T.jpg"
 
-# visible_path = "/home/srv-admin/images/items1/1313/20230807_131358202_V.jpg"
+visible_path = "/home/srv-admin/images/items1/1313/20230807_131358202_V.jpg"
 
 affine_matrix = np.array([[ 1.15775321e+00, 2.06036561e-02, -8.65530736e+01],
                         [-3.59868529e-02, 1.16843440e+00, -4.39524932e+01]])
@@ -68,19 +69,21 @@ for x1, y1, x2, y2 in bbox:
 #     ax.add_patch(plt.Polygon(i,fill=False))
 
 # detect_list = deque(maxlen=5)
-detect_dic = {}
-
+# detect_dic = {}
+df = pd.DataFrame(data=None, index=['time', 'place', 'item'])
 
 with open("example.txt", "w") as f:
     for pt in points:
         for poly, cls in zip(polygon, classes):
             if cv2.pointPolygonTest(poly, pt, False) >= 0:
                 # print(class_dic[cls])
-                detect_dic.setdefault(cls, 0)
-                detect_dic[cls] += 1
+                # detect_dic.setdefault(cls, 0)
+                # detect_dic[cls] += 1
+
+                df.append({'time': datetime.datetime.now(), 'place': 'table', 'item': class_dic[cls]})
                 # f.write(str(datetime.datetime.now()) + ", " + "at: table, " + class_dic[cls])
                 
-print(detect_dic[67])
+print(df)
 
 
 #         ax.scatter(pt[:1], pt[1:], color=color)
--- yolo/log_video.py
+++ yolo/log_video.py
@@ -32,8 +32,8 @@ def feature_compare(img1, img2):
 affine_matrix = np.array([[ 1.15775321e+00, 2.06036561e-02, -8.65530736e+01],
                         [-3.59868529e-02, 1.16843440e+00, -4.39524932e+01]])
 
-# path = '/home/srv-admin/images/items1/1313/*.jpg'
-path = r'C:\Users\tnkmo\Downloads\items1\items1\20230807_1313\*.jpg'
+path = '/home/srv-admin/images/items1/1313/*.jpg'
+# path = r'C:\Users\tnkmo\Downloads\items1\items1\20230807_1313\*.jpg'
 file_list = peekable(sorted(glob.iglob(path)))
 
 # fourcc = cv2.VideoWriter_fourcc('m', 'p', '4', 'v')
@@ -53,7 +53,7 @@ kernel = np.ones((5,5),np.uint8)
 model = YOLO("yolov8x.pt")
 class_dic = model.model.names
 
-detect_list = dict()
+# detect_list = dict()
 
 for i in file_list:
     try:
@@ -91,6 +91,8 @@ for i in file_list:
             bbox = pred[0].boxes.xyxy.cpu().numpy()
             classes = pred[0].boxes.cls.cpu().numpy()
 
+            
+
             # cv2.imshow("img", frame)
             # cv2.waitKey(1)
 
@@ -99,11 +101,11 @@ for i in file_list:
                 polygon.append(np.array([[x1, y1], [x1, y2], [x2, y2], [x2, y1]]))
 
             with open("example.txt", "a") as f:
-                for pt in points:
-                    for poly, cls in zip(polygon, classes):
+                for poly, cls in zip(polygon, classes):
+                    for pt in points:
                         if cv2.pointPolygonTest(poly, pt, False) >= 0:
                             # print(class_dic[cls])
-                            detect_list[cls]
+                            # detect_list[cls]
                             f.write(str(datetime.datetime.now()) + ", " + "place: table, " + class_dic[cls] + "\n")
             
 

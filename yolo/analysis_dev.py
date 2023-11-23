9ea994a282473ef16a9f811e0b5715482a7c9d0a
mtanaka1117 <u710058f@ecs.osaka-u.ac.jp>
2023-11-23 22:04:05 +0900

    軽微な変更

--- yolo/analysis_dev.py
+++ yolo/analysis_dev.py
@@ -1,11 +1,5 @@
 import pandas as pd
 import datetime
-import math
-
-def calc_dist(bbox1, bbox2):
-    dist1 = math.sqrt((bbox1[0]-bbox2[0])**2 + (bbox1[1]-bbox2[1])**2)
-    dist2 = math.sqrt((bbox1[2]-bbox2[2])**2 + (bbox1[3]-bbox2[3])**2)
-    return dist1+dist2
 
 csv_path = 'dummy.csv'
 df_dict = {}
@@ -17,17 +11,15 @@ with open(csv_path) as f:
         label = df.at[i, "label"].astype(int)
         
         #ラベルが無いなら追加
-        # df_dict.setdefault(label, df)
         if label not in label_dict.keys():
             df_dict[label] = df
             df_time = pd.to_datetime(df.at[i,'time'], format='%Y-%m-%d %H:%M:%S.%f')
-            label_dict[label] = [1, [float(c) for c in df.at[i,"bbox"].split(',')], df_time]
-            # print(df_bbox)
+            label_dict[label] = [1, [float(c) for c in df.at[i,"bbox"][1:-1].split(',')], df_time]
         
         #ラベルが存在するなら
         else:
             label_dict[label][0] += 1
-            df_bbox = [float(c) for c in df.at[i,"bbox"].split(',')]
+            df_bbox = [float(c) for c in df.at[i,"bbox"][1:-1].split(',')]
             bbox_avg = label_dict[label][1]
             n = label_dict[label][0]
             

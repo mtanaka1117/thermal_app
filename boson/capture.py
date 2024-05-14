1458fb725264c92fe7e1a391143a16c033ec5641
mtanaka1117 <u710058f@ecs.osaka-u.ac.jp>
2024-05-14 14:41:38 +0900

    edit capture.py

--- boson/capture.py
+++ boson/capture.py
@@ -4,7 +4,10 @@ os.environ["OPENCV_VIDEOIO_MSMF_ENABLE_HW_TRANSFORMS"] = "0"
 import cv2
 from flirpy.camera.boson import Boson
 import numpy as np
-import datetime
+import datetime as dt
+
+TEMP_MIN = -60.0
+TEMP_MAX = -40.0
 
 def hconcat_resize_min(im_list, interpolation=cv2.INTER_CUBIC):
     h_min = min(im.shape[0] for im in im_list)
@@ -17,24 +20,23 @@ cap_t = Boson()
 
 while True:
     img_t = cap_t.grab().astype(np.float32)
-    # Rescale to 8 bit
-    img_t = 255*(img_t - img_t.min())/(img_t.max()-img_t.min())
-    img_t = img_t.astype(np.uint8)
-    
     _, img_v = cap_v.read()
+    
+    # now = dt.datetime.now().strftime("%Y%m%d_%H%M%S%f")[:-3]
+    # with open('./thermal/{}_T.dat'.format(now), 'wb') as f:
+    #     f.write(img_t)
+    # cv2.imwrite('./thermal/{}_V.dat'.format(now), img_v)
+    
+    img = img.astype(np.uint16).reshape([512, 640])/100 - 273.15
+    img = 255.0*(img - TEMP_MIN)/(TEMP_MAX - TEMP_MIN)
+    img = img.astype(np.uint8)
     
     img_col = cv2.applyColorMap(img_t, cv2.COLORMAP_INFERNO)
     img_col = hconcat_resize_min([img_col, img_v, img_col])
     mergeImg = np.hstack((img_col, img_v))
     
     cv2.imshow('Boson', mergeImg)
-        
-    # now = datetime.datetime.now()
-    # filename_t = './img/' + now.strftime('%Y%m%d_%H%M%S%f') + '_T.jpg'
-    # filename_v = './img/' + now.strftime('%Y%m%d_%H%M%S%f') + '_V.jpg'
-    # cv2.imwrite(filename_t, img_t)
-    # cv2.imwrite(filename_v, img_v)
-    # img_t.dump()
+    
     if cv2.waitKey(1) == 27:
         break  # esc to quit
         
--- boson/webcam.py
+++ boson/webcam.py
@@ -3,10 +3,14 @@ os.environ["OPENCV_VIDEOIO_MSMF_ENABLE_HW_TRANSFORMS"] = "0"
 
 import cv2
 import sys
+import datetime as dt
 
 cap = cv2.VideoCapture(1)
-ret, img = cap.read()
-cv2.imwrite('test.jpg',img)
+_, img = cap.read()
+now = dt.datetime.now().strftime("%Y%m%d_%H%M%S%f")[:-3]
+with open('./thermal/{}_V.dat'.format(now), 'wb') as f:
+    f.write(img)
+cv2.imwrite('./thermal/test.jpg', img)
 
 # if not cap.isOpened():
 #     sys.exit()
